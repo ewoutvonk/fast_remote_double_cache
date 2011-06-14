ec621d38e170e413a62339de99da98c22583028d
Ewout Vonk <ewout@truvolabs.com>
2011-06-14 15:21:55 +0200

    bugfix

--- lib/fast_remote_double_cache.rb
+++ lib/fast_remote_double_cache.rb
@@ -1,2 +1,2 @@
 require "#{File.dirname(__FILE__)}/capistrano/recipes/deploy/scm/indirect_git"
-require "#{File.dirname(__FILE__)}/capistrano/recipes/deploy/scm/fast_remote_cache"
+require "#{File.dirname(__FILE__)}/capistrano/recipes/deploy/strategy/fast_remote_cache"
